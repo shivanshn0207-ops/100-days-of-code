85e00aec79cc9e7f98d6103c6897e360c74a7623
shivanshn0207-ops <shivanshn0207@gmail.com>
2026-08-18 14:16:18 +0530

    Add files via upload

---
+++ day8_code1.c
@@ -0,0 +1,49 @@
+// Q15: Write a program to input a character and check whether it is an uppercase alphabet, lowercase alphabet, digit, or special character.
+
+/*
+Sample Test Cases:
+Input 1:
+A
+Output 1:
+Uppercase alphabet
+
+Input 2:
+a
+Output 2:
+Lowercase alphabet
+
+Input 3:
+3
+Output 3:
+Digit
+
+Input 4:
+#
+Output 4:
+Special character
+
+*/
+#include <stdio.h>
+int main()
+{
+    char ch;
+    printf("Enter the Character:");
+    scanf("%c", &ch);
+    if (ch >= 'A' && ch <= 'Z')
+    {
+        printf("Upper-Case");
+    }
+    else if (ch >= 'a' && ch <= 'z')
+    {
+        printf("Lower-Case");
+    }
+    else if (ch >= '0' && ch <= '9')
+    {
+        printf("Digit");
+    }
+    else
+    {
+        printf("Special Character");
+    }
+    return 0;
+}
---
+++ day8_code2.c
@@ -0,0 +1,48 @@
+// Q16: Write a program to input three numbers and find the largest among them using if–else.
+
+/*
+Sample Test Cases:
+Input 1:
+3 7 5
+Output 1:
+Largest is 7
+
+Input 2:
+-1 -5 0
+Output 2:
+Largest is 0
+
+*/
+#include <stdio.h>
+int main()
+{
+    int a, b, c;
+    printf("Enter Three Numbers:");
+    scanf("%d, %d, %d", &a, &b, &c);
+
+    if (a > b)
+    {
+        if (a > c)
+        {
+            printf("'%d' is the Largest Number!", a);
+        }
+        else
+        {
+            printf("'%d' is the Largest Number!", c);
+        }
+    }
+
+    else
+    {
+        if (b > c)
+        {
+            printf("'%d' is the Largest Number!", b);
+        }
+        else
+        {
+            printf("'%d' is the Largest Number!", c);
+        }
+    }
+
+    return 0;
+}
